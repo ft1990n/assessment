bdb8dca6121a27a4b6ab8e4f6d0b99484d1bcafc
ft1990n <36789590+ft1990n@users.noreply.github.com>
2018-02-25 21:55:27 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -40,6 +40,7 @@ assessmentButton.onclick = () => {
         '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
         '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
         '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
+        '{userName}のいいところは優しさです。{userName}の優しい雰囲気や立ち振舞に多くの人が癒やされています。'
     ];
  function assessment(userName) {
     // 全文字のコード番号を取得してそれを足し合わせる
